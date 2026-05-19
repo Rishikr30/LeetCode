97a2187f2885976a1938dcdbd4ecd0fca00b288e
Rishi Kumar <rishikr30@outlook.com>
2026-05-19 05:53:47 +0530

    Time: 2 ms (91.75%), Space: 47.9 MB (19.23%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            // If the current element is non-zero
-            if (nums[i] != 0) {
-
-                // Swap the current element with
-                // the 0 at index 'count'
-                int temp = nums[i];
+        int count=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=0){
+                int temp= nums[i];
                 nums[i] = nums[count];
                 nums[count] = temp;
-
-                // Move 'count' pointer to 
-                // the next position
                 count++;
             }
         }
