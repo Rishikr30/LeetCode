ac264e6cd3651534d506b566c25533273647a197
Rishi Kumar <rishikr30@outlook.com>
2026-07-07 05:50:56 +0530

    Time: 1 ms (99.96%), Space: 77.4 MB (31.87%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,24 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
-        int currSum = 0;
-        int ngNum = nums[0];
-        int count = 0;
-
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]<0)
-                count++;
-                ngNum = Math.max(ngNum,nums[i]);
-
-            currSum += nums[i];
-            if(currSum < 0)
-                currSum = 0;
-            maxSum = Math.max(currSum, maxSum);
+       
+        int ms = nums[0];
+        int cs= nums[0];
+        for(int i=1; i<nums.length; i++){
+            cs =Math.max(nums[i], cs+nums[i]);
+            ms =Math.max(ms, cs);
         }
-        if(count == nums.length){
-            return ngNum;
-        }else{
-            return maxSum;
-        }
+        return ms;
     }
 }
